ecc58353a534de4561969490032838ba54507747
Damian Fallas <damian.fallas@digitaslbi.com>
2018-09-18 15:36:15 -0600

    Day 3: Adding BE components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className={styles.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Button color="primary">primary</Button>{' '}
+        </div>
       </div>
     );
   }
